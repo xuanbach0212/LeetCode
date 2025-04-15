41a53e5164c3e9ffb50d333d69623176321e97dc
xuanbach0212 <xuanbachdao0212@gmail.com>
2025-04-15 12:09:15 +0900

    304

--- c++/304.cpp
+++ c++/304.cpp
@@ -14,31 +14,35 @@ private:
 public:
     NumMatrix(vector<vector<int>> &matrix)
     {
-        prefixSumMatrix.resize(matrix.size(), vector<int>(matrix[0].size(), 0));
+        // add extra column and row 0,0 for not out of bound case
+        // [0, 0,  0,  0]
+        // [0, 3,  3,  4]
+        // [0, 8, 14, 18]
+        // [0, 9, 17, 21]
+        prefixSumMatrix = vector<vector<int>>(matrix.size() + 1, vector<int>(matrix[0].size() + 1, 0));
         for (int i = 0; i < matrix.size(); i++)
         {
-            int currentSumRow = 0;
+            int prefixSumRow = 0;
             for (int j = 0; j < matrix[i].size(); j++)
             {
-                currentSumRow += matrix[i][j];
-                prefixSumMatrix[i][j] = currentSumRow;
+                // prefix rectangle = prefix row + prefix above rectangle
+                prefixSumRow += matrix[i][j];
+                int prefixAboveRectangle = prefixSumMatrix[i][j + 1];
+                prefixSumMatrix[i + 1][j + 1] = prefixSumRow + prefixAboveRectangle;
             }
         }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2)
     {
-        int currRow = row1;
-        int sums = 0;
-        for (int row = row1; row <= row2; row++)
-        {
-            vector<int> prefixRow = prefixSumMatrix[currRow];
-            int left = (col1 > 0) ? prefixRow[col1 - 1] : 0;
-            int right = prefixRow[col2];
-            sums += (right - left);
-            currRow++;
-        }
-        return sums;
+        row1++, col1++, row2++, col2++;
+        // sum = bottom right - left column - above rectangle + top left
+        int bottomRight = prefixSumMatrix[row2][col2];
+        int left = prefixSumMatrix[row2][col1 - 1];
+        int above = prefixSumMatrix[row1 - 1][col2];
+        int topLeft = prefixSumMatrix[row1 - 1][col1 - 1];
+
+        return bottomRight - left - above + topLeft;
     }
 };
 
