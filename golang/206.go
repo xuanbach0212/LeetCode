7d5c83e496fc310e28f90adcf322516b3cdd2ca4
xuanbach0212 <xuanbachdao0212@gmail.com>
2025-10-21 10:16:39 +0900

    206

---
+++ golang/206.go
@@ -0,0 +1,22 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev, curr *ListNode
+	curr = head
+	prev = nil
+
+	for curr != nil {
+		tmp := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = tmp
+
+	}
+	return prev
+}
