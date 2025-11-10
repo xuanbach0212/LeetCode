8e260bd6de215c80baf0865a9abc26d74e510985
xuanbach0212 <xuanbachdao0212@gmail.com>
2025-11-10 11:39:57 +0900

    236

---
+++ golang/236.go
@@ -0,0 +1,74 @@
+//go:build ignore
+
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func BuildTree(vals []*int) *TreeNode {
+	if len(vals) == 0 || vals[0] == nil {
+		return nil
+	}
+
+	root := &TreeNode{Val: *vals[0]}
+	queue := []*TreeNode{root}
+	i := 1
+
+	for len(queue) > 0 && i < len(vals) {
+		node := queue[0]
+		queue = queue[1:]
+
+		if i < len(vals) && vals[i] != nil {
+			node.Left = &TreeNode{Val: *vals[i]}
+			queue = append(queue, node.Left)
+		}
+		i++
+
+		if i < len(vals) && vals[i] != nil {
+			node.Right = &TreeNode{Val: *vals[i]}
+			queue = append(queue, node.Right)
+		}
+		i++
+	}
+
+	return root
+}
+
+func intPtr(v int) *int { return &v }
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+	if right != nil {
+		return right
+	}
+	return left
+}
+
+func main() {
+	// root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+	vals := []*int{
+		intPtr(3), intPtr(5), intPtr(1), intPtr(6), intPtr(2), intPtr(0), intPtr(8), nil, nil, intPtr(7), intPtr(4),
+	}
+	root := BuildTree(vals)
+	result := lowestCommonAncestor(root, root.Left, root.Right)
+
+	fmt.Println(" lowestCommonAncestor is:", result.Val)
+}
