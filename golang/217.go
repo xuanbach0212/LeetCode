1234a57c4986619157c28e815ee068915f632cae
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-02-10 12:00:00 +0900

    217

---
+++ golang/217.go
@@ -0,0 +1,54 @@
+//go:build ignore
+
+package main
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,2,3,1]
+//
+// Output: true
+//
+// Explanation:
+//
+// The element 1 occurs at the indices 0 and 3.
+//
+// Example 2:
+//
+// Input: nums = [1,2,3,4]
+//
+// Output: false
+//
+// Explanation:
+//
+// All elements are distinct.
+//
+// Example 3:
+//
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+//
+// Output: true
+//
+//
+//
+// Constraints:
+//
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+func containsDuplicate(nums []int) bool {
+	set := make(map[int]struct{}, len(nums))
+	for _, num := range nums {
+		if _, ok := set[num]; ok {
+			return true
+		}
+		set[num] = struct{}{}
+	}
+	return false
+}
+
+func main() {
+}
