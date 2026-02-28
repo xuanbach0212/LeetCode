3aca699108d9727e9de3c133ea530779de19aad0
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-02-28 12:00:00 +0900

    28

---
+++ golang/28.go
@@ -0,0 +1,44 @@
+//go:build ignore
+
+package main
+
+// 28. Find the Index of the First Occurrence in a String
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//
+//
+//
+// Example 1:
+//
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+//
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+//
+//
+// Constraints:
+//
+// 1 <= haystack.length, needle.length <= 104
+// haystack and needle consist of only lowercase English characters.
+
+func strStr(haystack string, needle string) int {
+	for i := 0; i < len(haystack)-len(needle)+1; i++ {
+		if haystack[i] == needle[0] && (i+len(needle)-1) < len(haystack) {
+			if haystack[i:i+len(needle)] == needle {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+}
