1c3d97885a3868d2a423fa3519ff88bb2ed86d53
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-03-11 19:30:20 +0900

    230

---
+++ golang/230.go
@@ -0,0 +1,74 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func BuildTree(vals []*int) *TreeNode {
+	if len(vals) == 0 || vals[0] == nil {
+		return nil
+	}
+
+	root := &TreeNode{Val: *vals[0]}
+	queue := []*TreeNode{root}
+	i := 1
+
+	for len(queue) > 0 && i < len(vals) {
+		node := queue[0]
+		queue = queue[1:]
+
+		if i < len(vals) && vals[i] != nil {
+			node.Left = &TreeNode{Val: *vals[i]}
+			queue = append(queue, node.Left)
+		}
+		i++
+
+		if i < len(vals) && vals[i] != nil {
+			node.Right = &TreeNode{Val: *vals[i]}
+			queue = append(queue, node.Right)
+		}
+		i++
+	}
+
+	return root
+}
+
+func intPtr(v int) *int { return &v }
+
+// Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+//
+//
+//
+// Example 1:
+//
+//
+// Input: root = [3,1,4,null,2], k = 1
+// Output: 1
+// Example 2:
+//
+//
+// Input: root = [5,3,6,2,4,null,null,1], k = 3
+// Output: 3
+
+func kthSmallest(root *TreeNode, k int) int {
+	return
+}
+
+func main() {
+	// root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+	vals := []*int{
+		intPtr(3), intPtr(5), intPtr(1), intPtr(6), intPtr(2), intPtr(0), intPtr(8), nil, nil, intPtr(7), intPtr(4),
+	}
+	root := BuildTree(vals)
+	result := isValidBST(root)
+
+	fmt.Println("isValidBST is:", result)
+}
