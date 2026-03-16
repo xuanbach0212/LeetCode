00376d618010473c77f296a8927bb4ba942857ab
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-03-16 12:00:00 +0900

    169

---
+++ golang/169.go
@@ -0,0 +1,51 @@
+//go:build ignore
+
+package main
+
+//Given an array nums of size n, return the majority element.
+//
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [3,2,3]
+//Output: 3
+//Example 2:
+//
+//Input: nums = [2,2,1,1,1,2,2]
+//Output: 2
+//
+//
+//Constraints:
+//
+//n == nums.length
+//1 <= n <= 5 * 104
+//-109 <= nums[i] <= 109
+//The input is generated such that a majority element will exist in the array.
+//
+//
+//Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+func majorityElement(nums []int) int {
+	res := nums[0]
+	count := 0
+
+	for _, num := range nums {
+		if count == 0 {
+			res = num
+		}
+
+		if num == res {
+			count += 1
+		} else {
+			count -= 1
+		}
+	}
+
+	return res
+}
+
+func main() {
+}
