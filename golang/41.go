c76d26b6fc6fa5ea99229aea5a4107392e50f77b
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-03-21 12:00:00 +0900

    41

---
+++ golang/41.go
@@ -0,0 +1,51 @@
+//go:build ignore
+
+package main
+
+// Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+//
+// You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,2,0]
+// Output: 3
+// Explanation: The numbers in the range [1,2] are all in the array.
+// Example 2:
+//
+// Input: nums = [3,4,-1,1]
+// Output: 2
+// Explanation: 1 is in the array but 2 is missing.
+// Example 3:
+//
+// Input: nums = [7,8,9,11,12]
+// Output: 1
+// Explanation: The smallest positive integer 1 is missing.
+//
+//
+// Constraints:
+//
+// 1 <= nums.length <= 105
+// -231 <= nums[i] <= 231 - 1
+
+func firstMissingPositive(nums []int) int {
+	for i := 0; i < len(nums); i++ {
+		for nums[i] > 0 && nums[i] < len(nums)+1 && nums[nums[i]-1] != nums[i] {
+			targetIdx := nums[i] - 1
+			nums[targetIdx], nums[i] = nums[i], nums[targetIdx]
+		}
+	}
+
+	for i, num := range nums {
+		if num != i+1 {
+			return i + 1
+		}
+	}
+
+	return len(nums) + 1
+}
+
+func main() {
+}
