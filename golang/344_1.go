556523e7cc09dacde8933465e5f0c26d737d5865
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-03-30 12:00:00 +0900

    344

---
+++ golang/344_1.go
@@ -0,0 +1,36 @@
+//go:build ignore
+
+package main
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+//
+// You must do this by modifying the input array in-place with O(1) extra memory.
+//
+//
+//
+// Example 1:
+//
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+//
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+//
+//
+// Constraints:
+//
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+func reverseString(s []byte) {
+	l, r := 0, len(s)-1
+	for l < r {
+		s[l], s[r] = s[r], s[l]
+		l++
+		r--
+	}
+}
+
+func main() {
+}
