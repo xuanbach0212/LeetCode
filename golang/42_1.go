725416b62d8cb09e3f76f0f11691bdea79416677
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-04-01 15:28:46 +0900

    42

---
+++ golang/42_1.go
@@ -0,0 +1,44 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+)
+
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+//
+//
+//
+// Example 1:
+//
+//
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+// Example 2:
+//
+// Input: height = [4,2,0,3,2,5]
+// Output: 9
+
+func trap(height []int) int {
+	l, r := 0, len(height)-1
+	res := 0
+	maxL, maxR := height[l], height[r]
+	for l < r {
+		if height[l] < height[r] {
+			maxL = max(height[l], maxL)
+			res += maxL - height[l]
+			l++
+		} else {
+			maxR = max(height[r], maxR)
+			res += maxR - height[r]
+			r--
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println("trap is:", trap([]int{4, 2, 0, 3, 2, 5}))
+}
