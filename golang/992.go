868a0fbb692d0eca8a851a67a5b79b47e3242ed0
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-04-04 01:47:45 +0900

    992

---
+++ golang/992.go
@@ -0,0 +1,66 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+)
+
+// Given an integer array nums and an integer k, return the number of good subarrays of nums.
+//
+// A good array is an array where the number of different integers in that array is exactly k.
+//
+// For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+// A subarray is a contiguous part of an array.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,2,1,2,3], k = 2
+// Output: 7
+// Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+// Example 2:
+//
+// Input: nums = [1,2,1,3,4], k = 3
+// Output: 3
+// Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+//
+//
+// Constraints:
+//
+// 1 <= nums.length <= 2 * 104
+// 1 <= nums[i], k <= nums.length
+
+func subarraysWithKDistinct(nums []int, k int) int {
+	distict := 0
+	window := map[int]int{}
+	res := 0
+	for i := k; i <= len(nums); i++ {
+		l := 0
+		for r, num := range nums {
+			if window[num] == 0 {
+				distict++
+			}
+			window[num]++
+
+			if r-l+1 >= i {
+				if distict == k {
+					res += 1
+				}
+				window[nums[l]]--
+				if window[nums[l]] == 0 {
+					distict--
+				}
+				l++
+			}
+		}
+		window = map[int]int{}
+		distict = 0
+	}
+	return res
+}
+
+func main() {
+	fmt.Println("subarraysWithKDistinct is: ", subarraysWithKDistinct([]int{2, 1, 2, 1, 2}, 2))
+}
