f54d605cbd70b5653cb61f17561468e4b8414481
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-04-07 00:19:00 +0900

    71

---
+++ golang/71.go
@@ -0,0 +1,107 @@
+//go:build ignore
+
+package main
+
+import "strings"
+
+// You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
+//
+// The rules of a Unix-style file system are as follows:
+//
+// A single period '.' represents the current directory.
+// A double period '..' represents the previous/parent directory.
+// Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
+// Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
+// The simplified canonical path should follow these rules:
+//
+// The path must start with a single slash '/'.
+// Directories within the path must be separated by exactly one slash '/'.
+// The path must not end with a slash '/', unless it is the root directory.
+// The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
+// Return the simplified canonical path.
+//
+//
+//
+// Example 1:
+//
+// Input: path = "/home/"
+//
+// Output: "/home"
+//
+// Explanation:
+//
+// The trailing slash should be removed.
+//
+// Example 2:
+//
+// Input: path = "/home//foo/"
+//
+// Output: "/home/foo"
+//
+// Explanation:
+//
+// Multiple consecutive slashes are replaced by a single one.
+//
+// Example 3:
+//
+// Input: path = "/home/user/Documents/../Pictures"
+//
+// Output: "/home/user/Pictures"
+//
+// Explanation:
+//
+// A double period ".." refers to the directory up a level (the parent directory).
+//
+// Example 4:
+//
+// Input: path = "/../"
+//
+// Output: "/"
+//
+// Explanation:
+//
+// Going one level up from the root directory is not possible.
+//
+// Example 5:
+//
+// Input: path = "/.../a/../b/c/../d/./"
+//
+// Output: "/.../b/d"
+//
+// Explanation:
+//
+// "..." is a valid name for a directory in this problem.
+//
+//
+//
+// Constraints:
+//
+// 1 <= path.length <= 3000
+// path consists of English letters, digits, period '.', slash '/' or '_'.
+// path is a valid absolute Unix path.
+
+func simplifyPath(path string) string {
+	var sb strings.Builder
+	stack := []string{}
+	path += "/"
+	for i := 0; i < len(path); i++ {
+		if path[i] == '/' {
+			word := sb.String()
+			sb.Reset()
+			if word == ".." {
+				if len(stack) > 0 {
+					stack = stack[:len(stack)-1]
+				}
+			} else if word != "" && word != "." {
+				stack = append(stack, word)
+			}
+		} else {
+			sb.WriteByte(path[i])
+		}
+	}
+	res := strings.Join(stack, "/")
+	return "/" + res
+}
+
+func main() {
+}
