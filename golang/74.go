a4ede273d8790826ba440308b6d956698a396825
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-04-09 10:03:10 +0900

    74

---
+++ golang/74.go
@@ -0,0 +1,46 @@
+//go:build ignore
+
+package main
+
+import "fmt"
+
+func searchMatrix(matrix [][]int, target int) bool {
+	l, r := 0, len(matrix)-1
+
+	for l <= r {
+		mid := l + (r-l)/2
+
+		if matrix[mid][0] > target {
+			r = mid - 1
+		} else if matrix[mid][len(matrix[0])-1] < target {
+			l = mid + 1
+		} else {
+			break
+		}
+	}
+
+	if !(l <= r) {
+		return false
+	}
+
+	lt, rt := 0, len(matrix[l])-1
+	row := (l + r) / 2
+	for lt <= rt {
+		mt := lt + (rt-lt)/2
+
+		if matrix[row][mt] > target {
+			rt = mt - 1
+		} else if matrix[row][mt] < target {
+			lt = mt + 1
+		} else {
+			return true
+		}
+
+	}
+
+	return false
+}
+
+func main() {
+	fmt.Println(searchMatrix([][]int{{1, 3, 5, 7}, {10, 11, 6, 20}, {23, 30, 34, 60}}, 3))
+}
