c8b956a004bff9f0e6b13bc7a03634ab9b81bf69
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-04-16 09:44:47 +0900

    392

---
+++ golang/392.go
@@ -0,0 +1,51 @@
+//go:build ignore
+
+package main
+
+import "fmt"
+
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+//
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+//
+//
+//
+// Example 1:
+//
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+//
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+//
+//
+// Constraints:
+//
+// 0 <= s.length <= 100
+// 0 <= t.length <= 104
+// s and t consist only of lowercase English letters.
+//
+//
+// Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+
+func isSubsequence(s string, t string) bool {
+	if len(t) < len(s) {
+		return false
+	}
+
+	j := 0
+	for i := 0; i < len(t); i++ {
+		if j < len(s) {
+			if t[i] == s[j] {
+				j++
+			}
+		}
+	}
+	return j == len(s)
+}
+
+func main() {
+	fmt.Println(isSubsequence("abc", "ahbgdc"))
+	fmt.Println(isSubsequence("axc", "ahbgdc"))
+}
