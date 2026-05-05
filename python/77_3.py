4e56d06a56741a8498aa014f254b3a1dac552f73
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-05-06 02:09:43 +0900

    77

---
+++ python/77_3.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(i: int, sol: List[int]):
+            
+            
+
+        backtrack(1, [])
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.combine(4, 2))
+    print(s.combine(1, 1))
