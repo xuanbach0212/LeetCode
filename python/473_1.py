cc29884ce712849fbb6f7fd30badff253c7c03dd
xuanbach0212 <xuanbachdao0212@gmail.com>
2026-05-18 08:57:40 +0900

    473

---
+++ python/473_1.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        total = sum(matchsticks)
+        length = total // 4
+        if total % 4 != 0:
+            return False
+
+        matchsticks.sort(reverse=True)
+        sides = [0] * 4
+
+        def backtrack(i: int) -> bool:
+            if i == len(matchsticks):
+                return True
+
+            for j in range(4):
+                if sides[j] + matchsticks[i] <= length:
+                    sides[j] += matchsticks[i]
+                    if backtrack(i + 1):
+                        return True
+                    sides[j] -= matchsticks[i]
+
+                    if sides[j] == 0:
+                        break
+
+            return False
+
+        return backtrack(0)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.makesquare([1, 1, 2, 2, 2]))
+    print(s.makesquare([3, 3, 3, 3, 4]))
